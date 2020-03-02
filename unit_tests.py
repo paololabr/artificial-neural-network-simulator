94abcc41284e8b60262ab2989dc0be6999246fe7
Lucio Messina <luciotuttominuscolo@inventati.org>
2020-03-02 16:39:22 +0100

    move unit tests in unit_tests.py

---
+++ unit_tests.py
@@ -0,0 +1,107 @@
+
+import unittest
+
+import numpy as np
+from neural_network import *
+
+class TestNeuralNetwork (unittest.TestCase):
+
+    def test_forward_pass ( self ):
+        # a network that XORs its inputs
+        n = BaseNeuralNetwork ( hidden_layer_sizes=(2,), hidden_activation="threshold", momentum=0, alpha=0 )
+        weights = [
+                    np.array ([[ 1,    1  ],
+                               [ 1,    1  ],
+                               [-1.5, -0.5]]),
+                    np.array ([[-1  ],
+                               [ 1  ],
+                               [-0.5]])
+        ]
+        n.set_weights (weights)
+        X = np.array ([[0,0],
+                       [0,1],
+                       [1,0],
+                       [1,1]])
+        y = [[0], [1], [1], [0]]
+        predicted = n.predict (X)
+        for x, y_pred, y_true in zip (X,predicted, y):
+            if y_true[0] == 1:
+                self.assertGreater (y_pred[0], 0, "wrong XOR prediction for input {}".format(x))
+            else:
+                self.assertLess (y_pred[0], 0, "wrong XOR prediction for input {}".format(x))
+    
+    # TEST backpropagation based on: https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+    def test_matt_mazur ( self ):
+        n = BaseNeuralNetwork ( hidden_layer_sizes=(2,), hidden_activation="logistic", output_activation="logistic", max_iter=1, 
+                                warm_start=True, learning_rate_init=0.5, momentum=0, alpha=0 )
+        weights = [
+                    np.array ([[ .15, .25 ],
+                               [ .2 , .3  ],
+                               [ .35, .35 ]]),
+                    np.array ([[ .4 , .5  ],
+                               [ .45, .55 ],
+                               [ .6 , .6  ]])
+        ]
+        n.set_weights (weights)
+        X = np.array ([[0.05, 0.1]])
+        y = np.array ([[0.01, 0.99]])
+        predicted = n.predict (X)
+        losses = loss_functions["squared"] (y, predicted)
+        for x, p, loss in zip (X,predicted, losses):
+            self.assertAlmostEqual (sum(loss), 0.2983711083, 8, "Wrong loss")
+        n.fit (X, y)
+        expected_weights = [
+            np.array ([[ .149780716, .24975114  ],
+                       [ .19956143,  .29950229  ],
+                       [ .34561432,  .34502287  ]]),
+            np.array ([[ .35891648,  .51130127  ],
+                       [ .408666186, .561370121 ],
+                       [ .53075072,  .61904912  ]])
+        ]
+        self.assertTrue (np.allclose (n._weights, expected_weights), "Wrong weights after one step of backpropagation")
+
+    # TEST convergence on classification case (Xnor)
+    def test_classification(self):
+        n = BaseNeuralNetwork (hidden_layer_sizes=(30,), learning_rate_init=0.1, output_activation="zero_one_tanh", loss="log_loss",
+                               momentum=0, alpha=0)
+        X = [ [0, 0],
+              [0, 1],
+              [1, 0],
+              [1, 1]
+        ]
+        y = [ [ 1],
+              [ 0],
+              [ 0],
+              [ 1]
+        ]
+        n.fit (X, y)
+        predicted = n.predict (X)
+        losses = loss_functions["log_loss"] (y, predicted)
+        for x, p, loss in zip (X,predicted, losses):
+            self.assertLess ( sum(loss), 0.1, "Wrong predicted Xnor for input: {}".format(x) )
+        
+    def test_regression_linear_case ( self ):
+        # network that learns to compute sum of its inputs and double of the second input
+        n = BaseNeuralNetwork (hidden_layer_sizes=(50,), learning_rate_init=0.01, momentum=0, alpha=0)
+        X = [ 
+                [-0.55609785, -0.44237751, -1.51930792,  0.31342967],
+                [ 1.86589251, -0.64794613, -1.40532609,  0.19970042],
+                [ 2.07525975,  1.14612304, -1.21620428, -0.2127494 ],
+                [-0.9680726 ,  1.81546847, -0.71370392, -0.37450352]
+            ]
+        y = [
+                [-2.20435361, -0.88475503 ],
+                [ 0.0123207,  -1.29589226 ],
+                [ 1.79242911,  2.29224607 ],
+                [-0.24081157,  3.63093693 ]
+            ]
+        n.fit (X, y)
+        predicted = n.predict (X)
+        losses = loss_functions["squared"] (y, predicted)
+        for x, p, loss in zip (X,predicted, losses):
+            self.assertLess ( sum(loss), 0.1, "wrong (sum,double) prediction for input {}".format(x) )
+
+#unit tests
+if __name__ == "__main__":
+    
+    unittest.main ()
