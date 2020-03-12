9513671940267a1644b0a12eb02fb7b5c0188a60
Lucio Messina <luciotuttominuscolo@inventati.org>
2020-03-12 19:13:54 +0100

    grid_s.py specify as command line argument the number of models to test; changed possible values for parameters

--- grid_s.py
+++ grid_s.py
@@ -6,7 +6,13 @@ from utility import ReadData
 from utility import GridSearchCV
 from utility import getRandomParams
 
+import tqdm
+
 def main():
+
+    n_configurations = 10
+    if len(sys.argv) > 1:
+        n_configurations = int (sys.argv[1])
 
     data, labels, _, _ = ReadData("cup/ML-CUP19-TR.csv", 0.90)
     
@@ -14,20 +20,16 @@ def main():
         print ("Error reading data")
         exit()
 
-    nn = MLPRegressor(n_iter_no_change=10)
+    # nn = MLPRegressor(n_iter_no_change=10, max_iter=5)
+    nn = MLPRegressor(n_iter_no_change=10, max_iter=500)
     
-    params=[ {'hidden_layer_sizes': [(50,) ,(100,) ,(50,50)] , 'alpha': [0. , 0.001, 0.005, 0.01, 0.05], 'batch_size': [1 , 10 , 50 , 200, len(data)],
-         'learning_rate': ['constant'], 'learning_rate_init': [0.01, 0.05, 0.1], 'momentum': [0., 0.1, 0.7, 0.9],
+    params=[ {'hidden_layer_sizes': [(10,10), (20,), (50,) ,(100,), (50,50)] , 'alpha': [0., 0.05], 
+        'batch_size': [1, 5, 10, 50, 100, 'auto', 500, len(data)],
+         'learning_rate': ['constant', 'adaptive', 'linear'], 'learning_rate_init': [0.001, 0.1], 'momentum': [0., 0.9],
         'early_stopping': [True, False], 'activation': ['relu', 'tanh', 'logistic'] } ]
 
-    '''
-    params=[ {'hidden_layer_sizes': [(50,) ,(100,) ,(50,50)] , 'alpha': [0. , 0.001, 0.005, 0.01, 0.05], 'batch_size': [1 , 10 , 50 , 200, len(data)],
-         'learning_rate': ['linear', 'adaptive'], 'learning_rate_init': [0.02, 0.1, 0.2], 'momentum': [0., 0.1, 0.7, 0.9],
-        'early_stopping': [True, False], 'activation': ['relu', 'tanh', 'logistic'] } ]
-    '''
-
-    randparams = getRandomParams(params)
-    
-    ResList , minIdx = GridSearchCV(nn, randparams, data, labels, _euclidean_loss, 5, True)
+    for i in tqdm.tqdm (range (n_configurations), desc="configurations"):
+        randparams = getRandomParams(params)
+        ResList, minIdx = GridSearchCV(nn, randparams, data, labels, _euclidean_loss, 5, uniquefile=True, write_best=False)
 
 main()
