73b0157616e077b2239d26e7d8d61186eee5b3d1
Lucio Messina <luciotuttominuscolo@inventati.org>
2020-03-13 19:58:25 +0100

    best_gs_results tsv output

--- best_gs_results.py
+++ best_gs_results.py
@@ -16,11 +16,13 @@ def main ():
 
     print ("TOP {} RESULTS IN FOLDER {}/{}* :".format(n_best, folder, fileprefix))
 
+    print ("\t".join (["rank", "avg_loss", "std_loss", "n_folds"] + [key for key in sorted (results[0][0])]))
     for i, (params, perf) in enumerate (results, 1):
-        print ("#{}".format(i))
-        print ("params: {}".format(params))
-        print ("[avg_loss, std_loss, n_folds]: {}".format(perf))
-
+        # print ("#{}".format(i))
+        # print ("[avg_loss, std_loss, n_folds]: {}".format(perf))
+        
+        print ("\t".join ([str(i)] + [str(x) for x in perf] + [str(params[key]) for key in sorted (params)]))
+        
 
 if __name__ == "__main__":
     main()
