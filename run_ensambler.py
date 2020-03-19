fe491ae4046153ea296648ab3e4d18f3805d96b5
paolo <pielleunipi@gmail.com>
2020-03-19 23:01:28 +0100

    model added, inserted model number parameter

--- run_ensambler.py
+++ run_ensambler.py
@@ -1,4 +1,4 @@
-
+import sys
 from neural_network import *
 from utility import ReadData
 from ensambler import Ensambler
@@ -15,6 +15,7 @@ def main():
     constituent_params = [ 
         {'activation': 'logistic', 'alpha': 0.0005951915361345095, 'batch_size': 1, 'early_stopping': False, 'hidden_layer_sizes': [50, 50], 'learning_rate': 'adaptive', 'learning_rate_init': 0.054980020763198, 'momentum': 0.8050419419933234},
         {'activation': 'logistic', 'alpha': 0.0002188148237404819, 'batch_size': 1, 'early_stopping': False, 'hidden_layer_sizes': [50, 50], 'learning_rate': 'constant', 'learning_rate_init': 0.016437178740396977, 'momentum': 0.483140617180181},
+        {'activation': "logistic", "alpha": 0.0009044205421256557, "batch_size": 1, "early_stopping": False, "hidden_layer_sizes": [50, 50], "learning_rate": "adaptive", "learning_rate_init": 0.06205633095090332, "momentum": 0.01510303444629194},
         {'activation': 'logistic', 'alpha': 0.0017164735753597117, 'batch_size': 5, 'early_stopping': False, 'hidden_layer_sizes': [50, 50], 'learning_rate': 'linear', 'learning_rate_init': 0.051375660832639905, 'momentum': 0.5586182890198031},
         {'activation': 'logistic', 'alpha': 0.001542764264801877, 'batch_size': 5, 'early_stopping': False, 'hidden_layer_sizes': [50, 50], 'learning_rate': 'constant', 'learning_rate_init': 0.020231597719749344, 'momentum': 0.5704950000170622},
         {'activation': 'logistic', 'alpha': 0.0016753169392290158, 'batch_size': 5, 'early_stopping': False, 'hidden_layer_sizes': [50, 50], 'learning_rate': 'constant', 'learning_rate_init': 0.06569622348675933, 'momentum': 0.5544087332459164},
@@ -26,10 +27,17 @@ def main():
     ]
 
     models = []
-    for parameters_dict in constituent_params:
+    num_conf = len(constituent_params)
+    if (len(sys.argv) > 1) and  (0 < int(sys.argv[1]) < len(constituent_params)):
+        num_conf = int (sys.argv[1])
+    
+    for index, parameters_dict in enumerate(constituent_params):
         nn = MLPRegressor ()
         nn.set_params (**parameters_dict)
         models.append (nn)
+        if (index == (num_conf-1)):
+            break
+
     
     Xtrain, Xtest, ytrain, ytest = train_test_split (data, labels, shuffle=True)
     # print ("data.shape", data.shape)
